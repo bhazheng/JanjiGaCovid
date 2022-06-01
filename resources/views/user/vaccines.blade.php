df92ab4d7a1a0434f9f90e84334a17897d3eaa40
puterianandityaa <puteriamh@gmail.com>
2022-06-02 06:13:43 +0700

    add vaccines section to home

---
+++ resources/views/user/vaccines.blade.php
@@ -0,0 +1,64 @@
+<div class="page-section" id="jenisVaksin">
+    <div class="container">
+        <h1 class="text-center mb-5 wow fadeInUp" style="color:#fff; font-weight:bolder;">JENIS VAKSIN</h1>
+
+        <div class="owl-carousel wow fadeInUp" id="doctorSlideshow">
+            <div class="item">
+                <div class="card-doctor">
+                    <div class="header">
+                        <img src="../assets/img/vaksin.png" alt="">
+                    </div>
+                    <div class="body">
+                        <p class="text-xl mb-1">Sinovac</p>
+                        <span class="text-m text-grey">Efektivitas : 65,3%</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="card-doctor">
+                    <div class="header">
+                        <img src="../assets/img/vaksin.png" alt="">
+                    </div>
+                    <div class="body">
+                        <p class="text-xl mb-1">Astra Zenecca</p>
+                        <span class="text-m text-grey">Efektivitas : 62,1%</span>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card-doctor">
+                    <div class="header">
+                        <img src="../assets/img/vaksin.png" alt="">
+                    </div>
+                    <div class="body">
+                        <p class="text-xl mb-1">Sinopharm</p>
+                        <span class="text-m text-grey">Efektivitas : 78%</span>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card-doctor">
+                    <div class="header">
+                        <img src="../assets/img/vaksin.png" alt="">
+                    </div>
+                    <div class="body">
+                        <p class="text-xl mb-1">Moderna</p>
+                        <span class="text-m text-grey">Efektivitas : 86,4%-94,1%</span>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card-doctor">
+                    <div class="header">
+                        <img src="../assets/img/vaksin.png" alt="">
+                    </div>
+                    <div class="body">
+                        <p class="text-xl mb-1">Cansino</p>
+                        <span class="text-m text-grey">Efektivitas : 68,83%</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
